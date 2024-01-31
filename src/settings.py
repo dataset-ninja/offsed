c6eb5cf617ee049d6b25e788854647d87088891b
peixed351 <peixed351@gmail.com>
2024-01-31 18:48:17 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "OFFSED"
 PROJECT_NAME_FULL: str = "OFFSED: Off-Road Semantic Segmentation Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
